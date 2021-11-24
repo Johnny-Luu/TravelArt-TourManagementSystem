de6f51f9dbc34a14e858dee2756f90772317fe0d
Johnny-Luu <ngocsangluu58@gmail.com>
2021-11-24 22:04:08 +0700

    add new tour with image

--- GUI/ViewModels/AddingTourViewModel.cs
+++ GUI/ViewModels/AddingTourViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using BLL;
 using GUI.Views.Pages;
 using Microsoft.Win32;
@@ -10,17 +15,49 @@ namespace GUI.ViewModels
 {
     public class AddingTourViewModel : BaseViewModel
     {
+        private string _base64img = null;
+
         public PageAddingTour PgAddingTour { get; set; }
+        public ICommand ChooseImageCommand { get; set; }
         public ICommand PushTourCommand { get; set; }
-        
+
         public AddingTourViewModel()
         {
+            ChooseImageCommand = new RelayCommand<PageAddingTour>(para => true, para => ChooseImage(para));
             PushTourCommand = new RelayCommand<PageAddingTour>(para => true, para => PushTour(para));
+        }
+
+        public void ChooseImage(PageAddingTour para)
+        {
+            PgAddingTour = para;
+
+            var ofd = new OpenFileDialog();
+            ofd.Title = "Choose a picture";
+            ofd.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                         "Portable Network Graphic (*.png)|*.png";
+
+            if (ofd.ShowDialog() == true)
+            {
+                // preview img
+                PgAddingTour.ImgPicture.Stretch = Stretch.UniformToFill;
+                PgAddingTour.ImgPicture.Source = new BitmapImage(new Uri(ofd.FileName));
+
+                var file = ofd.FileName;
+                var bytes = System.IO.File.ReadAllBytes(file);
+                _base64img = Convert.ToBase64String(bytes);
+            }
         }
 
         public async void PushTour(PageAddingTour para)
         {
             PgAddingTour = para;
+
+            if (_base64img == null)
+            {
+                MessageBox.Show("Please choose a picture");
+                return;
+            }
             
             var tourBLL = new TourBLL();
 
@@ -29,33 +66,22 @@ namespace GUI.ViewModels
             var price = PgAddingTour.TbPrice.Text;
             var shortDescription = PgAddingTour.TbShortDescription.Text;
             
-            var ofd = new OpenFileDialog();
-            ofd.Title = "Choose a picture";
-            ofd.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
-                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
-                         "Portable Network Graphic (*.png)|*.png";
-            
-            if (ofd.ShowDialog() == true)
+            var tour = new TourModel
             {
-                var file = ofd.FileName;
-                var bytes = System.IO.File.ReadAllBytes(file);
-                var base64 = Convert.ToBase64String(bytes);
-            
-                var tour = new TourModel
-                {
-                    Id = id,
-                    Name = name,
-                    Price = price,
-                    Description = shortDescription,
-                    Img = base64,
-                    CurrentComment = 0,
-                    CurrentTour = 0,
-                    Rating = 0,
-                    Status = 1
-                };
+                Id = id,
+                Name = name,
+                Price = price,
+                Description = shortDescription,
+                Img = _base64img,
+                CurrentComment = 0,
+                CurrentTour = 0,
+                Rating = 0,
+                Status = 1
+            };
                 
-                tourBLL.PushTour(tour);
-            }
+            tourBLL.PushTour(tour);
+            MessageBox.Show("Successfully added");
         }
+
     }
 }
