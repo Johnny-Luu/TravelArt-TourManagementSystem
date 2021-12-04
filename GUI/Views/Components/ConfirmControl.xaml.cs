dde3ce8f0afd3c651899d66564532e6ceb2a3e4f
biyasto <biyasto@gmail.com>
2021-12-04 15:25:17 +0700

    GUI Confirm Page +fix scrollbar

---
+++ GUI/Views/Components/ConfirmControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GUI.Views.Components
+{
+    public partial class ConfirmControl : UserControl
+    {
+        public ConfirmControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseDown_Accept(object sender, MouseButtonEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MouseDown_Decline(object sender, MouseButtonEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- GUI/Views/Pages/PageConfirm.xaml.cs
+++ GUI/Views/Pages/PageConfirm.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using GUI.Views.Components;
 
 namespace GUI.Views.Pages
 {
@@ -6,7 +7,25 @@ namespace GUI.Views.Pages
     {
         public PageConfirm()
         {
+            var item1 = new ConfirmControl();
+            var item2 = new ConfirmControl();
+            var item3 = new ConfirmControl();
+            var item4 = new ConfirmControl();
+            var item9 = new ConfirmControl();
+            var item5 = new ConfirmControl();
+            var item6 = new ConfirmControl();
+            var item7 = new ConfirmControl();
+            var item8 = new ConfirmControl();
             InitializeComponent();
+            WpListConfirm.Children.Add(item1);
+            WpListConfirm.Children.Add(item2);
+            WpListConfirm.Children.Add(item3);
+            WpListConfirm.Children.Add(item4);
+            WpListConfirm.Children.Add(item5);
+            WpListConfirm.Children.Add(item6);
+            WpListConfirm.Children.Add(item7);
+            WpListConfirm.Children.Add(item8);
+            WpListConfirm.Children.Add(item9);
         }
     }
 }
