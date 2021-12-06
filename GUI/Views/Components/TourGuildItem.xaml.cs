c871242635acc3c947d982ff604a1ad0908fad4f
peterneverdie301 <lahuy3012002@gmail.com>
2021-12-06 17:02:02 +0700

    (GUI) Add PageTour_GroupTourList

---
+++ GUI/Views/Components/TourGuildItem.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GUI.Views.Components
+{
+    public partial class TourGuildItem : UserControl
+    {
+        public TourGuildItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GUI/Views/Pages/PageTour_GroupTourList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GUI.Views.Pages
+{
+    public partial class PageTour_GroupTourList : Page
+    {
+        public PageTour_GroupTourList()
+        {
+            InitializeComponent();
+        }
+    }
+}
