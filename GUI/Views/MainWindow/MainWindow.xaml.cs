06a73d7a67367d979092589ba65f8d0c735e66c2
Johnny-Luu <ngocsangluu58@gmail.com>
2021-12-14 16:56:40 +0700

    fix renamed icons asset bug

--- GUI/Views/MainWindow/MainWindow.xaml.cs
+++ GUI/Views/MainWindow/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             InitializeComponent();
             if (LbStatistic != null) LbStatistic.Style = Resources["NavItemClickedStyle"] as Style;
-            FrContainer.Content = new ProfileWindow();
+            FrContainer.Content = new PageStatistic();
         }
 
         private void BtnMinimize_OnMouseDown(object sender, MouseButtonEventArgs e)
